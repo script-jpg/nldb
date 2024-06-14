2a7e508332fa724ef8b262d8a0900f94735b83d2
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2024-06-14 00:10:18 -0400

    Fix top_k relevant strategy
    So it actually chooses top k documents and not just the top 2 entries that may be the same doc

--- DataUtils/find_relevant_strategy.py
+++ DataUtils/find_relevant_strategy.py
@@ -9,5 +9,9 @@ class FindTopK(FindRelevantStrategy):
     def execute(self, relevance):
         relevance.sort(key = lambda item: item['cos_similarity'], reverse=True)
         top_k = int(input("Please enter the most docs you'd like to match in this query: "))
-        relevance = relevance[:top_k]
-        return set([item['doc'] for item in relevance])
+        top_k_relevant = set()
+        for _, entry in enumerate(relevance):
+            if len(top_k_relevant) == top_k:
+                break
+            top_k_relevant.add(entry['doc'])
+        return top_k_relevant
